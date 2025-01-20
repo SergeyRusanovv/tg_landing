7faffa8c1e9ea2c3c85165ca59d1c79d7fec93b2
Sergey Rusanov <sergeikulyb@mail.ru>
2025-01-20 15:32:15 +0300

    Добавление логики передачи уведомления по электронной почте

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from config import AppConfig
+
+import smtplib
+from email.message import EmailMessage
 
 
 app = FastAPI()
@@ -22,10 +26,24 @@ async def submit(
     phone: str = Form(None),
     telegram: str = Form(None)
 ):
-    return {"message": "Форма успешно отправлена!", "data": {
-        "first_name": first_name,
-        "last_name": last_name,
-        "email": email,
-        "phone": phone,
-        "telegram": telegram
-    }}
+    send_email = EmailMessage()
+    send_email["Subject"] = "Здравствуйте"
+    send_email["From"] = AppConfig.SMTP_USER
+    send_email["To"] = email
+    send_email['Disposition-Notification-To'] = AppConfig.SMTP_USER
+
+    phone_number = phone if phone else "Вы не указали ваш номер телефона"
+    telegram_url = telegram if telegram else "Вы не указали ссылку на телеграмм"
+
+    send_email.set_content(
+        "<div>"
+        f'<h1 style="color: red;">Здравствуйте! {first_name} {last_name} 😊</h1><p>Ваши данные из рекламы получены. '
+        f'Email: {email}, Телефон: {phone_number}, Ссылка в телеграмм: {telegram_url}</p></div>',
+        subtype="html",
+    )
+
+    with smtplib.SMTP_SSL(AppConfig.SMTP_HOST, AppConfig.SMTP_PORT) as server:
+        server.login(AppConfig.SMTP_USER, AppConfig.SMTP_PASSWORD)
+        server.send_message(send_email)
+
+    return {"message": "Уведомления отправлены!"}
